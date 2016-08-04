f178509b7f1adfc898878c9ee0462c5b077859d6
Artur Welp <arturh@viavale.com.br>
2016-08-04 15:21:32 -0300

    Update calendar.class.php

--- local/googlecalendar/classes/calendar.class.php
+++ local/googlecalendar/classes/calendar.class.php
@@ -10,58 +10,9 @@
  * @copyright  2016 Univates - htttp://www.univates.br/
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 include_once 'google_service.class.php';
 
 class Calendar extends GoogleService {
-    
-    /*
-     * create_user_calendar
-     * 
-     * This function will create a calendar for a specific user.
-     * 
-     * @param (int) $userid - The target user
-     * 
-     */
-    function create_user_calendar($userid = null) {
-        global $DB, $CFG;
-
-        function __construct() {
-            parent::__construct();
-        }
-
-        if ($this->user_calendar_exist($userid)) {
-            return;
-        }
-
-        $sql = "SELECT * 
-                  FROM {user} 
-                 WHERE id = ?";
-        $result = $DB->get_record_sql($sql, array($userid));
-
-        $calendar = new Google_Service_Calendar_Calendar();
-
-        $calendar->setSummary( get_string('user', 'local_googlecalendar') . $result->username);
-        $calendar->setTimeZone($CFG->timezone);
-        $createdCalendar = $this->get_service()->calendars->insert($calendar);
-        $calendarId = $createdCalendar->getId();
-
-        /* Share the calendar */
-        $rule = new Google_Service_Calendar_AclRule();
-        $scope = new Google_Service_Calendar_AclRuleScope();
-
-        $scope->setType("user");
-        $scope->setValue($result->email);
-        $rule->setScope($scope);
-        $rule->setRole("reader");
-        $createdRule = $this->get_service()->acl->insert($calendarId, $rule);
-
-        $register = new stdClass();
-        $register->calendarid = $calendarId;
-        $register->userid = $userid;
-        $DB->insert_record("local_googlecalendar", $register);
-    }
-
     /*
      * user_calendar_exist
      * 
@@ -70,6 +21,7 @@ class Calendar extends GoogleService {
      * @param (int) $userid - Target user
      * @return (bollean) 
      */
+
     public function user_calendar_exist($userid = null) {
         global $DB;
 
@@ -86,6 +38,120 @@ class Calendar extends GoogleService {
     }
 
     /*
+     * create_user_calendar
+     * 
+     * This function will create a calendar for a specific user.
+     * 
+     * @param (int) $userid - The target user
+     * 
+     */
+
+    function create_user_calendar($userid = null) {
+        global $DB, $CFG;
+
+        function __construct() {
+            parent::__construct();
+        }
+
+        if ($this->user_calendar_exist($userid)) {
+            return true;
+        }
+
+        $sql = "SELECT * 
+                  FROM {user} 
+                 WHERE id = ?";
+        $result = $DB->get_record_sql($sql, array($userid));
+        try {
+            $calendar = new Google_Service_Calendar_Calendar();
+
+            $calendar->setSummary(get_string('user_prefix', 'local_googlecalendar') . $result->username . get_string('user_sufix', 'local_googlecalendar'));
+            $calendar->setTimeZone($CFG->timezone);
+            $createdcalendar = $this->get_service()->calendars->insert($calendar);
+            $calendarid = $createdcalendar->getId();
+
+            /* Share the calendar */
+            $rule = new Google_Service_Calendar_AclRule();
+            $scope = new Google_Service_Calendar_AclRuleScope();
+
+            $scope->setType("user");
+            $scope->setValue($result->email);
+            $rule->setScope($scope);
+            $rule->setRole("reader");
+            $createdRule = $this->get_service()->acl->insert($calendarid, $rule);
+
+            $register = new stdClass();
+            $register->calendarid = $calendarid;
+            $register->userid = $userid;
+            $register->active = 't';
+            $DB->insert_record("local_googlecalendar", $register);
+        } catch (Exception $ex) {
+            return false;
+        }
+        return true;
+    }
+
+    public function user_calendar_acl_create($userid = null) {
+        global $DB, $CFG;
+
+        function __construct() {
+            parent::__construct();
+        }
+
+        if (!$this->user_calendar_exist($userid)) {
+            return false;
+        }
+
+        $sql = "SELECT gc.id, u.email, gc.calendarid  
+                  FROM {user} u, {local_googlecalendar} gc
+                 WHERE u.id = ? AND
+                       u.id = gc.userid";
+        $result = $DB->get_record_sql($sql, array($userid));
+
+        /* Share the calendar */
+        $rule = new Google_Service_Calendar_AclRule();
+        $scope = new Google_Service_Calendar_AclRuleScope();
+
+        $scope->setType("user");
+        $scope->setValue($result->email);
+        $rule->setScope($scope);
+        $rule->setRole("reader");
+
+        $createdRule = $this->get_service()->acl->insert($result->calendarid, $rule);
+        
+        $register = new stdClass();
+        $register->id = $result->id;
+        $register->active = 't';
+
+        $DB->update_record('local_googlecalendar', $register, array());
+    }
+
+    public function user_calendar_acl_remove($userid = null) {
+        global $DB;
+
+        $sql = "SELECT id, calendarid
+                  FROM {local_googlecalendar} 
+                 WHERE userid = ?";
+        $result = $DB->get_record_sql($sql, array($userid));
+
+        $acl_list = $this->get_service()->acl->listAcl($result->calendarid);
+
+        $acl_id = '';
+        foreach ($acl_list->getItems() as $rule) {
+            try {
+                $this->get_service()->acl->delete($result->calendarid, $rule->getId());
+            } catch (Exception $exc) {
+                
+            }
+        }
+
+        $register = new stdClass();
+        $register->id = $result->id;
+        $register->active = 'f';
+
+        $DB->update_record('local_googlecalendar', $register, array());
+    }
+
+    /*
      * user_calendar_exist
      * 
      * This function will create a calendar for a specific course
@@ -93,44 +159,49 @@ class Calendar extends GoogleService {
      * @param (int) $courseid - Target course
      * 
      */
-    function create_course_calendar($courseid = null) {
+
+    public function create_course_calendar($courseid = null) {
         global $DB, $CFG;
 
         $sql = "SELECT * FROM {course} 
                  WHERE id = ?";
         $result = $DB->get_record_sql($sql, array($courseid));
 
-        $calendar = new Google_Service_Calendar_Calendar();
-        $calendar->setSummary($result->fullname);
-        $calendar->setTimeZone($CFG->timezone);
-        $createdcalendar = $this->get_service()->calendars->insert($calendar);
-        $calendarid = $createdcalendar->getId();
-        
+        try {
+            $calendar = new Google_Service_Calendar_Calendar();
+            $calendar->setSummary($result->fullname);
+            $calendar->setTimeZone($CFG->timezone);
+            $createdcalendar = $this->get_service()->calendars->insert($calendar);
+            $calendarid = $createdcalendar->getId();
+        } catch (Exception $exc) {
+            return false;
+        }
+
         $register = new stdClass();
-        
+
         //This is the two way sync magic
-        try {            
+        try {
             $channel = new Google_Service_Calendar_Channel($this->get_client());
             $channel->setId($courseid);
             $channel->setType('web_hook');
             $channel->setAddress($CFG->wwwroot . "/local/googlecalendar/event_watcher.php");
             $watchevent = $this->get_service()->events->watch($calendarid, $channel);
-            
-            
+
             $register->resourceid = $watchevent->resourceId;
             $register->resourceexpiration = $watchevent->expiration;
-            
         } catch (Exception $exc) {
             
         }
-        
+
         $register->calendarid = $calendarid;
         $register->courseid = $courseid;
         $register->synctokenlastdate = time();
 
         $DB->insert_record("local_googlecalendar", $register);
+
+        return true;
     }
-    
+
     /*
      * course_calendar_exist
      * 
@@ -139,6 +210,7 @@ class Calendar extends GoogleService {
      * @param (int) $courseid - Target user
      * @return (bollean) 
      */
+
     public static function course_calendar_exist($courseid = null) {
         global $DB;
         if ($courseid == null) {
@@ -156,8 +228,6 @@ class Calendar extends GoogleService {
         return false;
     }
 
-    
-
     /*
      * delete_course_calendar
      * 
@@ -166,6 +236,7 @@ class Calendar extends GoogleService {
      * @param (int) $courseid - Target course
      * 
      */
+
     public function delete_course_calendar($courseid = null) {
         global $DB;
 
@@ -194,6 +265,7 @@ class Calendar extends GoogleService {
      * @param (int) $courseid - Target course
      * 
      */
+
     function enrol_teacher($userid, $courseid) {
         global $DB;
 
@@ -202,6 +274,11 @@ class Calendar extends GoogleService {
                  WHERE courseid = ?";
 
         $result = $DB->get_record_sql($sql, array($courseid));
+
+        if (empty($result)) {
+            return false;
+        }
+
         $calendarId = $result->calendarid;
 
         $result = $DB->get_records("user", array('id' => $userid));
@@ -227,6 +304,7 @@ class Calendar extends GoogleService {
      * @param (int) $courseid - Target course
      * 
      */
+
     function unenrol_teacher($userid, $courseid) {
         global $DB;
 

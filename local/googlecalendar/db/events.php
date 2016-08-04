c93ed34dcf4ec354c706022c4966679d5eb3d65b
Artur Welp <arturh@viavale.com.br>
2016-08-04 15:23:26 -0300

    Update events.php

--- local/googlecalendar/db/events.php
+++ local/googlecalendar/db/events.php
@@ -9,32 +9,37 @@ $handlers = array(
 );
 
 // List of events_2 observers.
-
+/*
 $observers = array(
     array(
         'eventname' => 'core\event\course_created',
         'includefile' => 'local/googlecalendar/classes/observer.php',
-        'callback' => 'google_calendar_course_update_observer::google_calendar_course_created'
-    ),
-    array(
-        'eventname' => 'core\event\course_updated',
-        'includefile' => 'local/googlecalendar/classes/observer.php',
-        'callback' => 'google_calendar_course_update_observer::google_calendar_course_updated'
+        'callback' => 'google_calendar_course_observer::google_calendar_course_created'
     ),
     array(
         'eventname' => 'core\event\course_deleted',
         'includefile' => 'local/googlecalendar/classes/observer.php',
-        'callback' => 'google_calendar_course_update_observer::google_calendar_course_deleted'
+        'callback' => 'google_calendar_course_observer::google_calendar_course_deleted'
     ),
     array(
         'eventname' => 'core\event\role_assigned',
         'includefile' => 'local/googlecalendar/classes/observer.php',
-        'callback' => 'google_calendar_course_update_observer::google_calendar_role_assigned'
+        'callback' => 'google_calendar_course_observer::google_calendar_role_assigned'
     ),
     array(
         'eventname' => 'core\event\role_unassigned',
         'includefile' => 'local/googlecalendar/classes/observer.php',
-        'callback' => 'google_calendar_course_update_observer::google_calendar_role_unassigned'
+        'callback' => 'google_calendar_course_observer::google_calendar_role_unassigned'
+    ),
+    array(
+        'eventname' => 'core\event\user_created',
+        'includefile' => 'local/googlecalendar/classes/observer.php',
+        'callback' => 'google_calendar_course_observer::google_calendar_user_created'
+    ),
+    array(
+        'eventname' => 'core\event\user_deleted',
+        'includefile' => 'local/googlecalendar/classes/observer.php',
+        'callback' => 'google_calendar_course_observer::google_calendar_user_deleted'
     )
-);
-
+);*/
+//take a look at http://stackoverflow.com/questions/26341243/moodle-service-hooks-and-integrating-with-another-system

34051606b47d913bf266a3c67746dfdeaf62aa92
Artur Welp <arturh@viavale.com.br>
2016-08-04 15:24:54 -0300

    Update local_googlecalendar.php

--- local/googlecalendar/lang/en/local_googlecalendar.php
+++ local/googlecalendar/lang/en/local_googlecalendar.php
@@ -3,4 +3,19 @@
 $string['pluginname'] = 'Google Calendar';
 $string['pluginnamedesc'] = 'Google Calendar';
 
-$string['user'] = 'User';
+$string['user_prefix'] = 'User: ';
+$string['user_sufix'] = " Calendar";
+
+$string['calendar_configuration'] = "Calendar Configuration";
+$string['user_configuration'] = 'User configuration';
+$string['status'] = 'Status: ';
+
+$string['bond_exist_and_active'] = 'Bond exist and is active.';
+$string['bond_exist_but_inactive'] = 'Bond exist, but is inactive.';
+$string['bond_not_exist'] = 'Bond do not exist.';
+$string['bond_in_processing'] = 'Your request is being processed.';
+
+$string['bond_create'] = "Create Bond";
+$string['bond_delete'] = "Delete Bond";
+$string['bond_activate'] = "Activate Bond";
+$string['bond_deactivate'] = "Deactivate Bond";

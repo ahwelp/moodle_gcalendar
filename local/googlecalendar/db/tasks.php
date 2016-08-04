a6cb324b18f900b7668328e7a34029356f6593fd
Artur Welp <arturh@viavale.com.br>
2016-08-04 15:24:24 -0300

    Create tasks.php

---
+++ local/googlecalendar/db/tasks.php
@@ -0,0 +1,13 @@
+<?php
+
+$tasks = array(
+    array(
+        'classname' => 'local_googlecalendar\task\sync_calendars_task', 
+        'blocking' => 0,
+        'minute' => '*',
+        'hour' => '*',
+        'day' => '*',
+        'dayofweek' => '*',
+        'month' => '*'
+    )    
+);

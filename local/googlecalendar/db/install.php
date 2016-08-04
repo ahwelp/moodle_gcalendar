2bd83a311b2023deae124f3740b2ad5e127a8144
Artur Welp <arturh@viavale.com.br>
2016-08-04 15:58:14 -0300

    Update install.php

--- local/googlecalendar/db/install.php
+++ local/googlecalendar/db/install.php
@@ -1,9 +1,12 @@
 <?php
-//If it don't woks, just make it by hand
-if( $DB->get_record('config', array('name'=>'calendar')) ){
-  $sql = "UPDATE {config} SET value = ? WHERE name = 'calendar'";
-  $DB->execute($sql, '../local/googlecalendar/');
-}else{
-  $sql = "INSERT INTO {config} (value, name) VALUES (?, ?)";
-  $DB->execute($sql, '../local/googlecalendar/');
+
+function xmldb_local_googlecalendar_install() {
+    global $DB;
+    if ($DB->get_record('config', array('name' => 'calendar'))) {
+        $sql = "UPDATE {config} SET value = ? WHERE name = 'calendar'";
+        $DB->execute($sql, array('../local/googlecalendar/'));
+    } else {
+        $sql = "INSERT INTO {config} (value, name) VALUES (?, ?)";
+        $DB->execute($sql, array('../local/googlecalendar/', 'calendar'));
+    }
 }
